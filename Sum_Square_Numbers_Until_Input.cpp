a33f091888ea07fb2ea79423e0fe9d2f439e3495
fxalban08 <francisco.alban@epn.edu.ec>
2020-06-02 18:57:10 -0500

    Preparing the output

--- Sum_Square_Numbers_Until_Input.cpp
+++ Sum_Square_Numbers_Until_Input.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 
 int main(){
+	
 	int x;
 	cout<<"Enter a number: "<<flush;
 	cin>>x;
+	
+	cout<<"The sum of the squares of all the positive numbers below the entered one is ";
 	return 0;
 }
